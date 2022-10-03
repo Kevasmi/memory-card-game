1e122d1e68920f6fd10a2fa94006e4e87a00c1f0
Richard Lasco <lasco9@gmail.com>
2022-10-03 14:14:10 -0500

    Added Header and MemoryCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import MemoryCard from './components/MemoryCard';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <main className='gameboard'>
+        <MemoryCard></MemoryCard>
+      </main>
+    </div>
+  );
+}
 
 export default App;
