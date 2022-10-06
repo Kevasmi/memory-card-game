ad980b30f4ef50961640ce0cbcd8bee40e3dbb87
Richard Lasco <lasco9@gmail.com>
2022-10-06 17:14:59 -0500

    Created score state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import React, { useState } from 'react';
 import Header from './components/Header';
 import CardList from './components/CardList';
+import cardData from './components/utilities/cardData';
 
 function App() {
+  const [score, setScore] = useState(0);
   const [cards, setCards] = useState(cardData);
 
   function handleCardClick(e) {
@@ -11,6 +13,7 @@ function App() {
       const newCard = cards.map((card) => {
         if (card.id === e.currentTarget.id) {
           if (card.clicked === false) {
+            setScore((prevState) => prevState + 1);
             return { ...card, clicked: true };
           } else {
             console.log('Game Over');
