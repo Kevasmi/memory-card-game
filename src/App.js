b6c61ceb28477b215dd86387172bee9ccac981a1
Richard Lasco <lasco9@gmail.com>
2022-10-06 17:14:42 -0500

    Lifted state up to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import CardList from './components/CardList';
 
 function App() {
+  const [cards, setCards] = useState(cardData);
+
+  function handleCardClick(e) {
+    setCards(() => {
+      const newCard = cards.map((card) => {
+        if (card.id === e.currentTarget.id) {
+          if (card.clicked === false) {
+            return { ...card, clicked: true };
+          } else {
+            console.log('Game Over');
+          }
+        }
+        return card;
+      });
+      return [...newCard];
+    });
+  }
+
   return (
     <div>
       <Header></Header>
       <main className='gameboard'>
-        <CardList />
+        <CardList onClick={handleCardClick} cardData={cards} />
       </main>
     </div>
   );
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,19 +1,12 @@
 import { Grid } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import MemoryCard from './MemoryCard';
-import cardData from './utilities/cardData';
 
 export default function CardList(props) {
-  const [cards, setCards] = useState(cardData);
+  const cards = props.cardData;
   const cardArray = cards.map((card) => (
     <Grid item key={card.id} xs={12} sm={6} md={4} lg={3} xl={2}>
-      <MemoryCard
-        id={card.id}
-        card={card}
-        onClick={() => {
-          console.log(card.name + ' is clicked. ' + card.id);
-        }}
-      />
+      <MemoryCard id={card.id} card={card} onClick={props.onClick} />
     </Grid>
   ));
 
