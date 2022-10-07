be9b54beb0306c582e835a3bc79d1765d85de701
Richard Lasco <lasco9@gmail.com>
2022-10-07 16:03:15 -0500

    Added TransitionsModal to App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import './App.css';
 import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Header from './components/Header';
+import TransitionsModal from './components/Modal';
 import CardList from './components/CardList';
 import cardData from './components/utilities/cardData';
 
 function App() {
   const [score, setScore] = useState(0);
   const [cards, setCards] = useState(cardData);
+  const [open, setOpen] = useState(false);
+  const [victory, setVictory] = useState(false);
 
   function handleCardClick(e) {
     setCards(() => {
       const newCard = cards.map((card) => {
+        if (score === 11) {
+          setScore(12);
+          setVictory(true);
+          handleOpen();
+        }
         if (card.id === e.currentTarget.id) {
           if (card.clicked === false) {
             setScore((prevState) => prevState + 1);
             return { ...card, clicked: true };
           } else {
-            console.log('Game Over');
+            handleOpen();
           }
         }
         return card;
@@ -26,9 +34,32 @@ function App() {
     });
   }
 
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  async function handleReset() {
+    handleClose();
+    await timeoutResolver(100);
+    setScore(0);
+    setCards(cardData);
+    setVictory(false);
+  }
+
+  function timeoutResolver(ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve(true);
+      }, ms);
+    });
+  }
+
   return (
     <div>
       <Header></Header>
+      <TransitionsModal
+        victory={victory}
+        open={open}
+        handleReset={handleReset}
+      ></TransitionsModal>
       <main className='gameboard'>
         <Typography
           variant='h6'
